01f5ec1b69e165c8b6e014fdedab118629e6589a
meirshoulkin <meirshoulkin@gmail.com>
2024-09-11 20:42:14 +0300

    psvm

--- src/main/java/SQL_meirbase.java
+++ src/main/java/SQL_meirbase.java
@@ -1,3 +1,9 @@
-public class SQL_meirbase {
+import org.openqa.selenium.WebDriver;
 
+public class SQL_meirbase {
+    public static void main(String[] args) {
+        public void count(WebDriver driver) {
+
+        }
+    }
 }
